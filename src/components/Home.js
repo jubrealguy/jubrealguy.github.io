b4bcc50aa4e34a6b19f85d8672bc6ee932dd1b99
jubrealguy <whizkidjubrealguy@gmail.com>
2023-05-18 22:45:16 +0100

    added a section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import About from "./About"
+import Projects from "./Projects";
 
 const Home = () => {
     return (
         <main className="main">
             <About />
+            <Projects />
         </main>
     )
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,36 @@
+const Projects = () => {
+    return (
+        <section className="project">
+            <h2>Projects I have worked on</h2>
+            <p>I have worked on a lots of personal projects to develop myself as seen on my github but these are the ones I have worked with lately</p>
+            <div>
+                <a href="##" className="project__link">
+                    <h3>LinkTree</h3>
+                    <p>This is my first project with HNG. This project was built with REACT, it is a link tree that links to Zuri and HNG products for designs, coding and programming skills</p>
+                </a>
+                <a href="##" className="project__link">
+                    <h3>LinkTree</h3>
+                    <p>This is my first project with HNG. This project was built with REACT, it is a link tree that links to Zuri and HNG products for designs, coding and programming skills</p>
+                </a>
+                <a href="##" className="project__link">
+                    <h3>LinkTree</h3>
+                    <p>This is my first project with HNG. This project was built with REACT, it is a link tree that links to Zuri and HNG products for designs, coding and programming skills</p>
+                </a>
+                <a href="##" className="project__link">
+                    <h3>LinkTree</h3>
+                    <p>This is my first project with HNG. This project was built with REACT, it is a link tree that links to Zuri and HNG products for designs, coding and programming skills</p>
+                </a>
+                <a href="##" className="project__link">
+                    <h3>LinkTree</h3>
+                    <p>This is my first project with HNG. This project was built with REACT, it is a link tree that links to Zuri and HNG products for designs, coding and programming skills</p>
+                </a>
+                <a href="##" className="project__link">
+                    <h3>LinkTree</h3>
+                    <p>This is my first project with HNG. This project was built with REACT, it is a link tree that links to Zuri and HNG products for designs, coding and programming skills</p>
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default Projects;
