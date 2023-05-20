5c83fa53241500b81e49635d6e898be63ad5c6c8
jubrealguy <whizkidjubrealguy@gmail.com>
2023-05-20 22:54:17 +0100

    animation

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -2,10 +2,10 @@ import js from '../img/JS.png'
 import bootstrap from '../img/bootstrap.png'
 import C from '../img/C.png'
 import react_img from '../img/react.png'
-import sass from '../img/sass.png'
+import sass from '../img/Sass.png'
 import css3 from '../img/css3.png'
 import html from '../img/html.jpg'
-import python from '../img/python.png'
+import python from '../img/python.jpg'
 
 
 const Tools = () => {
@@ -14,14 +14,14 @@ const Tools = () => {
             <h2>Tools I have worked with.</h2>
             <p className="tool__paragraph">Other programming languages and tools that I am very proficient with.</p>
             <div className='tool__img-box'>
-                <img src={html} alt="" className="tool__img" />
-                <img src={css3} alt="" className="tool__img" />
-                <img src={js} alt="" className="tool__img" />
-                <img src={C} alt="" className="tool__img" />
-                <img src={python} alt="" className="tool__img" />
-                <img src={bootstrap} alt="" className="tool__img" />
-                <img src={sass} alt="" className="tool__img" />
-                <img src={react_img} alt="" className="tool__img" />
+                <img src={html} alt="" className="tool__img left" />
+                <img src={css3} alt="" className="tool__img right" />
+                <img src={js} alt="" className="tool__img left" />
+                <img src={C} alt="" className="tool__img right" />
+                <img src={python} alt="" className="tool__img left" />
+                <img src={bootstrap} alt="" className="tool__img right" />
+                <img src={sass} alt="" className="tool__img left" />
+                <img src={react_img} alt="" className="tool__img right" />
             </div>
         </section>
     )
