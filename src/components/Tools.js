d7f6631c043ada28fb786247a92a5741a990169a
jubrealguy <whizkidjubrealguy@gmail.com>
2023-06-18 09:22:05 +0100

    fixed the header

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -6,6 +6,7 @@ import sass from '../img/Sass.png'
 import css3 from '../img/css3.png'
 import html from '../img/html.jpg'
 import python from '../img/python.jpg'
+import sql from '../img/sql.png'
 
 
 const Tools = () => {
@@ -14,14 +15,15 @@ const Tools = () => {
             <h2>Tools I have worked with.</h2>
             <p className="tool__paragraph">Programming languages and tools that I am very proficient with.</p>
             <div className='tool__img-box'>
-                <img src={html} alt="" className="tool__img fast" />
-                <img src={css3} alt="" className="tool__img medium" />
-                <img src={js} alt="" className="tool__img slow" />
-                <img src={C} alt="" className="tool__img fast" />
-                <img src={python} alt="" className="tool__img medium" />
-                <img src={bootstrap} alt="" className="tool__img slow" />
-                <img src={sass} alt="" className="tool__img fast" />
-                <img src={react_img} alt="" className="tool__img medium" />
+                <img src={html} alt="html" className="tool__img fast" />
+                <img src={css3} alt="css" className="tool__img medium" />
+                <img src={js} alt="js" className="tool__img slow" />
+                <img src={C} alt="c" className="tool__img fast" />
+                <img src={python} alt="python" className="tool__img medium" />
+                <img src={bootstrap} alt="bootstrap" className="tool__img slow" />
+                <img src={sass} alt="sass" className="tool__img fast" />
+                <img src={react_img} alt="react" className="tool__img medium" />
+                <img src={sql} alt="sql" className="tool__img slow" />
             </div>
         </section>
     )
