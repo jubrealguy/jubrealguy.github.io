a746822e79d68d3f9afb4226b61e7a97145dcd92
jubrealguy <whizkidjubrealguy@gmail.com>
2024-05-31 14:18:07 +0100

    worked on the navigation and made all links to be target=_blank

--- src/components/About.js
+++ src/components/About.js
@@ -8,9 +8,9 @@ const About = () => {
            </div>
            <div className='about__content'>
                 <h1 className='about__header'>Hi, I'm <span>Adebayo Jubreel</span></h1>
-                <p className='about__paragraph'>I am a <span>Software Developer</span> that specializes in frontend development. I have built a solid passion for coding over the years. I am available for frontend engineering projects. You can connect with me on <a href="https://www.twitter.com/debayo_realest">Twitter</a></p>
+                <p className='about__paragraph'>I am a <span>Software Developer</span> that specializes in frontend development. I have built a solid passion for coding over the years. I am available for frontend engineering projects. You can connect with me on <a href="https://www.twitter.com/debayo_realest" target='_blank' rel='noreferrer'>Twitter</a></p>
                 <div className="link-box">
-                    <a href="https://www.github.com/jubrealguy" className="about__link">View my Github</a>
+                    <a href="https://www.github.com/jubrealguy" className="about__link" target='_blank' rel='noreferrer'>View my Github</a>
                 </div>
            </div>
         </section>
